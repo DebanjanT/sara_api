4f15531bbf567547dfa6bf3f3bc80df01d5597b4
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2022-02-16 02:50:11 +0530

    git add

--- api/views.py
+++ api/views.py
@@ -2,15 +2,12 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from api.db_constant import DB_get_public_room_info
 
-
+# api -> views
 # Create your views here.
+
 
 def public_room(request):
     public_room_info = DB_get_public_room_info
     return JsonResponse({
         'public_room': public_room_info
     })
-
-
-def __reaction__(request):
-    pass
